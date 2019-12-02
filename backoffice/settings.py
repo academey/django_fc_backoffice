6511df6c70d17f002544cf2f4cfe5ffd072fc460
academey <academey@gmail.com>
2019-12-02 15:28:41 +0900

    Add fastadmin app & DATABASE settings & Install mysqlclient

--- backoffice/settings.py
+++ backoffice/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+INSTALLED_APPS += [
+    'fastadmin',
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'backoffice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'employees',
+        'USER': 'root',
+        'PASSWORD': 'pass',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
