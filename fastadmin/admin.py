c4c9a785f2cb2d55c70c3c7c7567a666c0c9c907
academey <academey@gmail.com>
2019-12-02 16:14:25 +0900

    Add rangefilter to filtering

--- fastadmin/admin.py
+++ fastadmin/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Employees
+from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 
-admin.site.register(Employees)
+
+class DisplayEmployee(admin.ModelAdmin):
+    list_display = ('emp_no', 'first_name', 'last_name', 'gender', 'birth_date', 'hire_date')
+    search_fields = ['first_name']
+    list_filter = ('gender', ('birth_date', DateRangeFilter))
+
+    ordering = ('last_name', 'hire_date')
+
+
+admin.site.register(Employees, DisplayEmployee)
