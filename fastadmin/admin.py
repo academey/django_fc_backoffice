e5d9d05ab86fa168e890b0e60823fa36fbb63fc7
academey <academey@gmail.com>
2019-12-04 08:58:17 +0900

    has_add_permission & has_delete_permission

--- fastadmin/admin.py
+++ fastadmin/admin.py
@@ -7,8 +7,16 @@ class DisplayEmployee(admin.ModelAdmin):
     list_display = ('emp_no', 'first_name', 'last_name', 'gender', 'birth_date', 'hire_date')
     search_fields = ['first_name']
     list_filter = ('gender', ('birth_date', DateRangeFilter))
+    readonly_fields = ['emp_no', 'birth_date', 'gender']
 
     ordering = ('last_name', 'hire_date')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 
 admin.site.register(Employees, DisplayEmployee)
